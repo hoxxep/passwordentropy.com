f7ff41d6f0aa7be6d9252aa146c950e10db1a869
Liam Gray <hoxxep@gmail.com>
2026-01-04 19:11:10 +0000

    Fix the metadata tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://isitworththetime.com',
+    site: 'https://passwordentropy.com',
 
     compressHTML: import.meta.env.PROD,
 
